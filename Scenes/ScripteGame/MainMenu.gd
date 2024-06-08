4f727ae7f078dc140a12b7f2a9b9d24612a911b4
yannis300307 <yannis300307@gmail.com>
2024-06-08 11:46:42 +0200

    Fix des transitions

--- Scenes/ScripteGame/MainMenu.gd
+++ Scenes/ScripteGame/MainMenu.gd
@@ -2,14 +2,14 @@ extends Node2D
 
 func _on_texture_button_button_down():
 	GAME.new_save()
-	get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
+	Transit.change_scene_to_file("res://Scenes/MainMenu.tscn")
 
 
 
 func _ready():
 	if GAME.first_co and GAME.real_first_co:
 		GAME.real_first_co = false
-		get_tree().change_scene_to_file("res://assets/cinematique/cinematique_start.tscn")
+		get_tree().change_scene_to_file.bind("res://assets/cinematique/cinematique_start.tscn").call_deferred()
 	elif GAME.first_co:
 		DialogueManager.show_example_dialogue_balloon(load("res://dialogue/start.dialogue"), "start")
 		GAME.change_first_co(false)
--- assets/cinematique/CinematiqueStart.gd
+++ assets/cinematique/CinematiqueStart.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
 var state = 0
-# Called when the node enters the scene tree for the first time.
 
 
 func _process(_delta):
