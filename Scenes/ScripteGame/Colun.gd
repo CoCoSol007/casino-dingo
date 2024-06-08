842d98a772195e44615c5b36f2f9cda378934c95
yannis300307 <yannis300307@gmail.com>
2024-06-08 12:19:46 +0200

    Ajout preload des assets de la machine à sous + fixs

--- Scenes/ScripteGame/Colun.gd
+++ Scenes/ScripteGame/Colun.gd
@@ -12,6 +12,8 @@ var run = false
 @onready var sprite = $Sprite2D
 var win_image = null
 
+var preloaded_ressources = []
+
 signal finished
 signal pass_
 
@@ -20,7 +22,14 @@ func _ready():
 	GAME.connect("start_game", _start)
 	sprite.position.y = 0
 	$AnimationPlayer.play("stop")
-	
+	preload_ressources()
+
+func preload_ressources():
+	preloaded_ressources.append(load("res://assets/baloon.png"))
+	preloaded_ressources.append(load("res://assets/Cherry.png"))
+	preloaded_ressources.append(load("res://assets/forest.png"))
+	preloaded_ressources.append(load("res://assets/UwU.png"))
+	preloaded_ressources.append(load("res://assets/Money.png"))
 
 func _gen_list():
 	list = []
@@ -30,46 +39,29 @@ func _gen_list():
 		
 	
 func _process(_delta):
-
-	
 	if run :
-		if index ==  end_index -1 :
+		if index == end_index -1 :
 			run = false
 			$AnimationPlayer.play("stop")
-
 		else:
 			$AnimationPlayer.play("slide")
 
-	
-		
-	
 func update_image():
 	sprite.set_texture(get_image())
 	sprite.set_global_scale(Vector2(0.15,0.15))
 	
 func get_image(num = false):
 	var ret = "book"
-
-	if list[index] == 0:
-		ret = "baloon"
-	elif list[index] == 2:
-		ret = "Cherry"
-	elif list[index] == 4:
-		ret = "forest"
-	elif list[index] == 1:
-		ret = "UwU"
-	elif list[index] == 3:
-		ret = "Money"
 	
 	if !num:
-		ret = load("res://assets/" + ret + ".png")
+		ret = preloaded_ressources[list[index]]
 	return ret
 
 func _start():
 	speed = 6
-	$AnimationPlayer.set_speed_scale(speed)	
+	$AnimationPlayer.set_speed_scale(speed)
 	index = 0
-	_gen_list()	
+	_gen_list()
 	run = true
 
 
